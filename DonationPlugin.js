9b97df0cfbf8e2aebbeda5b4d9001ac41dcf5643
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-12-17 21:44:14 +0100

    Update DonationPlugin.js

--- DonationPlugin.js
+++ DonationPlugin.js
@@ -8,6 +8,7 @@ class DonationPlugin {
     onLoad() {
         console.log(`\x1b[34mINFO\x1b[0m | Hi there! If you want to support GreenFrogMCBE you can donate at: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`)
         console.log(`\x1b[34mINFO\x1b[0m | You will also be listed in our Discord server!`)
+        console.log(`\x1b[34mINFO\x1b[0m | If you do not want to support us just delete the DonationPlugin.js from plugins folder`)
     }
 
     onJoin(server, client) { }
