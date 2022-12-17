411487f56c26d3e941acedcc0589586e24318af7
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-12-17 21:53:14 +0100

    Update DonationReminder.js

--- DonationReminder.js
+++ DonationReminder.js
@@ -6,9 +6,9 @@ class DonationReminder {
     }
 
     onLoad() {
-        console.log(`\x1b[34mINFO\x1b[0m | Hi there! If you want to support GreenFrogMCBE you can donate at: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`)
-        console.log(`\x1b[34mINFO\x1b[0m | You will also be listed in our Discord server!`)
-        console.log(`\x1b[34mINFO\x1b[0m | If you do not want to support us just delete the DonationPlugin.js from plugins folder`)
+        console.log(`\x1b[34mINFO\x1b[0m | [DonationReminder] Hi there! If you want to support GreenFrogMCBE you can donate at: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`)
+        console.log(`\x1b[34mINFO\x1b[0m | [DonationReminder] You will also be listed in our Discord server!`)
+        console.log(`\x1b[34mINFO\x1b[0m | [DonationReminder] If you do not want to support us just delete the DonationPlugin.js from plugins folder`)
     }
 
     onJoin(server, client) { }
