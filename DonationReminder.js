c05d96a468ac0aa38a8181238cedbf366b554e6f
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-12-18 09:11:14 +0100

    Updated to gmc 1.1

--- DonationReminder.js
+++ DonationReminder.js
@@ -4,24 +4,33 @@ class DonationReminder {
     getName() {
         return "DonationReminder"
     }
-
+    
+    getServerVersion() {
+        return "1.1"
+    }
+    
+    getVersion() {
+        return "1.0"
+    }
+    
     onLoad() {
         console.log(`\x1b[34mINFO\x1b[0m | [DonationReminder] Hi there! If you want to support GreenFrogMCBE you can donate at: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`)
         console.log(`\x1b[34mINFO\x1b[0m | [DonationReminder] You will also be listed in our Discord server!`)
         console.log(`\x1b[34mINFO\x1b[0m | [DonationReminder] If you do not want to support us just delete the DonationPlugin.js from plugins folder`)
     }
 
-    onJoin(server, client) { }
-    onResourcePackInfoSent(server, client) { }
-    onPlayerHasNoResourcePacksInstalled(server, client) { }
-    onResourcePacksRefused(server, client) { }
-    onPlayerHaveAllPacks(server, client) { }
-    onResourcePacksCompleted(server, client) { }
-    onKick(server, client, msg) { }
-    onPlayerSpawn(server, client) { }
-    onChat(server, client, msg, fullmsg) { }
-    onCommand(server, client, command) { }
-    onInternalServerError(server, client, error) { }
+    onJoin() { }
+    onResourcePackInfoSent() { }
+    onPlayerHasNoResourcePacksInstalled() { }
+    onResourcePacksRefused() { }
+    onPlayerHaveAllPacks() { }
+    onResourcePacksCompleted() { }
+    onKick() { }
+    onPlayerSpawn() { }
+    onChat() { }
+    onCommand() { }
+    onConsoleCommand() { }
+    onInternalServerError() { }
 }
 
 module.exports = DonationReminder;
