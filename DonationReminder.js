25dfa74aebb62b18e3549c51e820431c3f246a80
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-12-22 18:30:17 +0100

    Update DonationReminder.js

--- DonationReminder.js
+++ DonationReminder.js
@@ -1,36 +1,32 @@
-class DonationReminder {
-    constructor() {}
+const BasePlugin = require("../src/api/BasePlugin");
+const Logger = require("../src/console/Logger");
 
-    getName() {
-        return "DonationReminder"
-    }
-    
-    getServerVersion() {
-        return "1.2"
-    }
-    
-    getVersion() {
-        return "1.0"
-    }
-    
-    onLoad() {
-        console.log(`\x1b[34mINFO\x1b[0m | [DonationReminder] Hi there! If you want to support GreenFrogMCBE you can donate at: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`)
-        console.log(`\x1b[34mINFO\x1b[0m | [DonationReminder] You will also be listed in our Discord server!`)
-        console.log(`\x1b[34mINFO\x1b[0m | [DonationReminder] If you do not want to support us just delete the DonationPlugin.js from plugins folder`)
-    }
+class DonationReminder extends BasePlugin {
+  getName() {
+    return "DonationReminder";
+  }
 
-    onJoin() { }
-    onResourcePackInfoSent() { }
-    onPlayerHasNoResourcePacksInstalled() { }
-    onResourcePacksRefused() { }
-    onPlayerHaveAllPacks() { }
-    onResourcePacksCompleted() { }
-    onKick() { }
-    onPlayerSpawn() { }
-    onChat() { }
-    onCommand() { }
-    onConsoleCommand() { }
-    onInternalServerError() { }
+  getServerVersion() {
+    return "1.2";
+  }
+
+  getVersion() {
+    return "1.0";
+  }
+
+  onLoad() {
+    Logger.prototype.log(
+      `[DonationReminder] Hi there! If you want to support GreenFrogMCBE you can donate at: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`,
+      "info"
+    );
+    Logger.prototype.log(
+      `[DonationReminder] You will also be listed in our Discord server!`,
+      "info"
+    );
+    Logger.prototype.log(
+      `[DonationReminder] If you do not want to support us just delete the DonationPlugin.js from plugins folder`
+    );
+  }
 }
 
 module.exports = DonationReminder;
