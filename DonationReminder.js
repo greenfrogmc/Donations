cc35f67add88c4c5f2b7d5fd7e9c8ac78e62fa19
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-12-24 14:23:33 +0100

    Updated

--- DonationReminder.js
+++ DonationReminder.js
@@ -7,7 +7,7 @@ class DonationReminder extends BasePlugin {
   }
 
   getServerVersion() {
-    return "1.2";
+    return "1.3";
   }
 
   getVersion() {
@@ -16,12 +16,10 @@ class DonationReminder extends BasePlugin {
 
   onLoad() {
     Logger.prototype.log(
-      `[DonationReminder] Hi there! If you want to support GreenFrogMCBE you can donate at: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`,
-      "info"
+      `[DonationReminder] Hi there! If you want to support GreenFrogMCBE you can donate at: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`
     );
     Logger.prototype.log(
-      `[DonationReminder] You will also be listed in our Discord server!`,
-      "info"
+      `[DonationReminder] You will also be listed in our Discord server!`
     );
     Logger.prototype.log(
       `[DonationReminder] If you do not want to support us just delete the DonationPlugin.js from plugins folder`
