7e9122aa8cf692b3d69a90dfaa814c8fb9390105
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2023-03-07 18:33:23 +0700

    string interpolation

--- Donations.js
+++ Donations.js
@@ -17,21 +17,22 @@ const fs = require("fs");
 module.exports = {
 	onLoad() {
 		const config = yaml.load(fs.readFileSync("config.yml", "utf8"));
+		const donateLink = "https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ";
 		switch (config.lang) {
 			case "fr_FR":
-				Logger.log(`Donations > Si vous avez trouvé ce projet utile, vous pouvez le soutenir ici: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`);
+				Logger.log(`Donations > Si vous avez trouvé ce projet utile, vous pouvez le soutenir ici: ${donateLink}`);
 				break;
 			case "vi_VN":
-				Logger.log(`Donations > Chào bạn! Nếu bạn muốn hỗ trợ GreenFrogMCBE, bạn có thể quyên góp tại: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`);
+				Logger.log(`Donations > Chào bạn! Nếu bạn muốn hỗ trợ GreenFrogMCBE, bạn có thể quyên góp tại: ${donateLink}`);
 				break;
 			case "lt_LT":
-				Logger.log(`Donations > Jei šis projektas jums pasirodė naudingas, galite jį paremti čia: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`);
+				Logger.log(`Donations > Jei šis projektas jums pasirodė naudingas, galite jį paremti čia: ${donateLink}`);
 				break;
 			case "uk_UA":
-				Logger.log(`Donations > Якщо ви вважаєте цей проект корисним, ви можете підтримати його тут: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`);
+				Logger.log(`Donations > Якщо ви вважаєте цей проект корисним, ви можете підтримати його тут: ${donateLink}`);
 				break;
 			default:
-				Logger.log(`Donations > If you found this project useful, you can support it here: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`);
+				Logger.log(`Donations > If you found this project useful, you can support it here: ${donateLink}`);
 				break;
 		}
 	},
